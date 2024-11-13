96591d5f5653bcb2f2259586643bb32a218e7b30
Muhammad Ansari <ansari.freelance@gmail.com>
2024-11-13 19:16:41 +0530

    Update Routes.js

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '../components/Login';
-import Attendance from '../pages/Attendance';
 import LandingPage from '../pages/LandingPage';
 import ForgotPassword from '../components/ForgotPassword';
 import Dashboard from '../components/Dashboard'; // Assume a Dashboard component exists
 import Students from '../components/Students';
 import Attendance from '../components/Attendance';
-import Fees from '../components/Fees';
-import Courses from '../components/Courses';
-import Exams from '../components/Exams';
 
 const AppRoutes = () => (
     <Router>
@@ -21,9 +17,6 @@ const AppRoutes = () => (
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/students" element={<Students />} />
             <Route path="/attendance" element={<Attendance />} />
-            <Route path="/fees" element={<Fees />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/exams" element={<Exams />} />
             {/* <Route path="/" element={<Login />} /> 
              Add other routes for courses, exams, etc. */}
         </Routes>
